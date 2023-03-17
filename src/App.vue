68b059a6c0b3c7cf81a757d8a2de9b489670da8a
Joshua77 <jpakinola@gmail.com>
2023-03-17 05:48:41 +0100

    Vue Altschool Counter

--- src/App.vue
+++ src/App.vue
@@ -24,3 +24,39 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+template
+<div class = "flex items-center justify-center gap-x-4">
+    button class = "px-4 py-2 text-white bg-blue-600 focus:outline-none"
+    click "increment"
+    increment
+    button
+    {{ number }}
+
+    button class = "px-4 py-2 text-white bg-red-600 focus:outline-none"
+    click "decrement"
+    Decrement
+    button
+    div
+
+    template
+
+
+    script
+    import { ref } from 'vue';
+    export default {
+    setup(){
+        const number = ref(0);
+
+        function increment(){
+        number.value++;
+        }
+
+        function decrement(){
+        number.value--;
+        }
+     return { number, increment, decrement };
+    },
+
+    };
+    script
