8b0fba284461b2603c81d371572f8158a2a9befa
Joshua77 <jpakinola@gmail.com>
2023-03-18 11:35:23 +0100

    Continued Work on the vue counter

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <section className="footer">
+    <div>
+      © 2023 Built by
+      <a href="https://github.com/Joshua77"> Joshua Akinola</a>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Footer",
+  methods: {},
+};
+</script>
