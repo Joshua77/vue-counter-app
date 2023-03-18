6c480ff53666f1cc6990cd7dd9e33310860d7610
Joshua77 <jpakinola@gmail.com>
2023-03-18 23:06:46 +0100

    fixinf build and deployment issues

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "./App.css";
 import router from './routes';
-import counterStore from './composables/counterStore';
-
+import counterStore from './Composables/counterStore';
 createApp(App).use(router).use(counterStore).mount('#app')
