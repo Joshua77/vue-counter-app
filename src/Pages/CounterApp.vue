fcdfa9ca133fed6b7ec029a5548d6c8e998e12d8
Joshua Akinola <46546654+Joshua77@users.noreply.github.com>
2023-03-18 23:43:41 +0100

    Update CounterApp.vue

--- src/Pages/CounterApp.vue
+++ src/Pages/CounterApp.vue
@@ -29,7 +29,7 @@
 </template>
   
 <script>
-    import useCounter from '@/composables/counter'
+    import useCounter from '@/Composables/counter'
     import { NavBar, Footer } from "../components";
     
     export default {
